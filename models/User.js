a6944668a1756cdfc706878fba0bf36a9bea9435
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-01-30 19:52:30 -0300

    verificador de senhas

--- models/User.js
+++ models/User.js
@@ -1,43 +1,58 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
-const User = sequelize.define('User', {
-    id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
-    },
-    nomeReal: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    nomeUsuario: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    telefone: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    genero: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    dto: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-    },
-    password: {
-        type: DataTypes.STRING,
-        allowNull: true,
-    },
-}, {
-    tableName: 'users',   // Nome da tabela no banco
-    timestamps: true,     // Cria os campos "createdAt" e "updatedAt"
-});
+module.exports = (sequelize) => {
+    const User = sequelize.define(
+        'User',
+        {
+            id: {
+                type: DataTypes.INTEGER,
+                autoIncrement: true,
+                primaryKey: true,
+            },
+            nomeReal: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            nomeUsuario: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            telefone: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            genero: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            dto: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+            password: {
+                type: DataTypes.STRING,
+                allowNull: false,
+            },
+        },
+        {
+            hooks: {
+                beforeCreate: async (user) => {
+                    const salt = await bcrypt.genSalt(10);
+                    user.password = await bcrypt.hash(user.password, salt);
+                },
+            },
+        }
+    );
 
-module.exports = User;
+    // Método para verificar senha
+    User.prototype.validPassword = async function (password) {
+        return bcrypt.compare(password, this.password);
+    };
+
+    return User;
+};
