e94bb20c98bc9afa70bc3225808b9d91ce3765de
Rein4ldoAlv3s <naldos15@hotmail.com>
2025-03-20 19:39:45 -0300

    atributos pagamento

--- models/Pagamento.js
+++ models/Pagamento.js
@@ -1,11 +1,21 @@
 const sequelize = require('../config/database');
+const { DataTypes } = require('sequelize');
 
 const Pagamento = sequelize.define(
     'Pagamento',
     {
-
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        // pix ou boleto
+        tipo_pagamento: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        }
     }
 );
 
 
-module.exports = Endereco;
+module.exports = Pagamento;
